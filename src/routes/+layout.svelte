d406bd68be0d1a5c19951897284b28939d73335b
MohamedM-Dev <mahde042004@gmail.com>
2026-04-22 01:18:14 +0300

    أضافة زر الرجوع

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { App } from '@capacitor/app';
+	
+	App.addListener('backButton', ({ canGoBack }) => {
+		if (canGoBack) {
+			// Navigate back if there is history
+			window.history.back();
+		} else {
+			// Exit the app if on the root page
+			App.exitApp();
+		}
+	});
+
 
 	let { children } = $props();
 </script>
